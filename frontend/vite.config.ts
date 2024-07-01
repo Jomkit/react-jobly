e31ced781b9d07154eeda0af7b1ad20ed8648481
Jomkit <j.jujaroen96@gmail.com>
2024-07-01 13:31:16 -0400

    initial commit, set up backend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: './src/__tests__/setup.ts',
+  }
+})
